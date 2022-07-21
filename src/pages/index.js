df4354ccb69c2e3780afcaae188761d98d18a9b6
Umang G <toumangg@gmail.com>
2022-07-21 17:07:10 +0530

    Under development
    Update the site content to show that site is under development

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,10 @@
 import React from "react"
 
 export default function Home() {
-    return <h1>Welcome to Umang Gala's personal website!</h1>
+    return (
+        <div>
+        <h1>Welcome to Umang Gala's personal website!!!</h1>
+        <h2>The site is under development</h2>
+        </div>
+    );
 }
